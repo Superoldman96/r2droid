dc377f69069701a1c90f3da6d4db4e9462d21e9b
wsdx233 <18082792459@163.com>
2026-02-19 21:14:57 +0800

    修改版本号，发布v0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "top.wsdx233.r2droid"
         minSdk = 24
         targetSdk = 26
-        versionCode = 2602151
-        versionName = "0.1.7"
+        versionCode = 2602190
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
